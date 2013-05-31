bb21dd20a72fe96ddc1ac87ad9cb186f8d1dbe9b
Sylvain Coulombel <scoulomb>
2013-06-01 00:34:59 +0200

    Ajout de l'agent lumière et exemple d'interfacage fonctionnel avec le port série arduino

--- src/LightSensorAgent/LightSensorBehaviour.java
+++ src/LightSensorAgent/LightSensorBehaviour.java
@@ -1,33 +1,129 @@
 package LightSensorAgent;
 
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import processing.app.Preferences;
+
+
 import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.introspection.ACLMessage;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
 
-class LightSensorBehaviour extends Behaviour{
+
+// teste avec l'arduino marche niquel !!
+class LightSensorBehaviour extends CyclicBehaviour implements SerialPortEventListener {
+	
+	SerialPort serialPort;
+	    /** The port we're normally going to use. */
+	private static final String PORT_NAMES[] = { 
+			"/dev/tty.usbserial-A9007UX1", // Mac OS X
+			"/dev/ttyUSB0", // Linux
+			"COM5", // Windows
+	};
+	/**
+	* A BufferedReader which will be fed by a InputStreamReader 
+	* converting the bytes into characters 
+	* making the displayed results codepage independent
+	*/
+	private BufferedReader input;
+	/** The output stream to the port */
+	private OutputStream output;
+	/** Milliseconds to block while waiting for port open */
+	private static final int TIME_OUT = 2000;
+	/** Default bits per second for COM port. */
+	private static final int DATA_RATE = 9600;
+	private String inputLine;
+		
+	
+	LightSensorBehaviour(){
+		CommPortIdentifier portId = null;
+		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+		//First, Find an instance of serial port as set in PORT_NAMES.
+		while (portEnum.hasMoreElements()) {
+			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+			for (String portName : PORT_NAMES) {
+				if (currPortId.getName().equals(portName)) {
+					portId = currPortId;
+					break;
+				}
+			}
+		}
+		if (portId == null) {
+			System.out.println("Could not find COM port.");
+			return;
+		}
+
+		try {
+			// open serial port, and use class name for the appName.
+			serialPort = (SerialPort) portId.open(this.getClass().getName(),
+					TIME_OUT);
+
+			// set port parameters
+			serialPort.setSerialPortParams(DATA_RATE,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+
+			// open the streams
+			input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+			output = serialPort.getOutputStream();
+
+			// add event listeners
+			serialPort.addEventListener(this);
+			serialPort.notifyOnDataAvailable(true);
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+		
+	}
 
 	@Override
 	public void action() {
 		// TODO Auto-generated method stub
+		System.out.println(inputLine);
 		jade.lang.acl.ACLMessage msg=myAgent.receive();
 		if(msg!=null){
-			System.out.println("AppAgent receive : "+ msg.getContent());
-			readValue();
+			System.out.println(inputLine);
+		}else{
+			block();
 		}
-		else block();
+	
 		
+	
 	}
 
-	private void readValue() {
-		System.out.println("toto");
-		
-	}
 
-	@Override
-	public boolean done() {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * Handle an event on the serial port. Read the data and print it.
+	 */
+	public synchronized void serialEvent(SerialPortEvent oEvent) {
+		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+			try {
+				inputLine=input.readLine();
+				// System.out.println(inputLine);
+			} catch (Exception e) {
+				//System.err.println(e.toString());
+			}
+		}
+		// Ignore all the other eventTypes, but you should consider the other ones.
 	}
+		
+		
+	
 
 	
 	
