1085e54f71c85af6d14db6c8e1d21907f8b52e11
Nicolas Biehler <biehler.nicolas@gmail.com>
2013-06-13 18:37:14 +0200

    add files Constants + LightBhv

---
+++ src/Data/Constants.java
@@ -0,0 +1,9 @@
+package Data;
+
+public class Constants {
+
+	/*
+	 * Number of lights that can subscribe on AutoSwitch
+	 */
+	public static final int NBR_MAX_LIGHTS = 3;
+}
---
+++ src/smarthouse/lightagent/LightSubscribeBehaviour.java
@@ -0,0 +1,49 @@
+package smarthouse.lightagent;
+
+import java.io.IOException;
+
+import Data.MessageContent;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+@SuppressWarnings("serial")
+public class LightSubscribeBehaviour extends Behaviour {
+	boolean end = false;
+	
+	@Override
+	public void action() {
+		MessageTemplate template = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);		
+		ACLMessage answer = myAgent.receive(template);
+			
+		if (answer != null) {
+			ObjectMapper objectMapper = new ObjectMapper();
+			MessageContent d = null;
+			try {
+				d = objectMapper.readValue(answer.getContent(), MessageContent.class);
+			} catch (JsonParseException e1) {
+				e1.printStackTrace();
+			} catch (JsonMappingException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+			System.out.println("I am registered : " + d.getValue());
+			end = true;
+		}
+		else {
+			block();
+		}
+	}
+
+	@Override
+	public boolean done() {
+		return end;
+	}
+}
