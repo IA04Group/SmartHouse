78f23f50efcef2db9f9caab3fee3288b9c0bf34e
Nicolas Biehler <biehler.nicolas@gmail.com>
2013-06-19 19:58:35 +0200

    push sysout autoswitch

--- src/smarthouse/autoswitchagent/AutoSwitchRequestsBehaviour.java
+++ src/smarthouse/autoswitchagent/AutoSwitchRequestsBehaviour.java
@@ -24,7 +24,7 @@ public class AutoSwitchRequestsBehaviour extends CyclicBehaviour {
 		
 		if (message != null) {
 			MessageContent d = new MessageContent(message);
-
+			System.out.println("message etait pas nul");
 			
 			/*
 			 * algo:
@@ -47,8 +47,9 @@ public class AutoSwitchRequestsBehaviour extends CyclicBehaviour {
 				return;
 			}
 			
-			
+			System.out.println("On a recu un truc");
 			if(d.getType().equals(Constants.LIGHT_SENSOR_AGENT)) {
+				System.out.println("Et la c'est bien light sensor agent");
 				isDay = this.isDay(d);
 			}
 			
